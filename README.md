414cc67fed58960369fd81c9ff57ee894f4f83f5
Jussi Virkkala <jussivirkkala@hotmail.com>
2021-03-01 11:55:06 +0200

    Update

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # R
 
-Simple R scripts for different purposes.  NO RESPONSIBILITY TAKEN FOR USE!
+Different R scripts. NO RESPONSIBILITY TAKEN FOR USE!
 
-# [hs-koronavirus-avoindata](hs-koronavirus-avoindata/)
+## [hs-koronavirus-avoindata](hs-koronavirus-avoindata/)
 
-![](hs-koronavirus-avoindata/tapaukset.gif)
+Analyzing (https://github.com/HS-Datadesk/koronavirus-avoindata) data about COVID-19 in Finland.
+
+![](hs-koronavirus-avoindata/tapaukset-Kaikki&#32;sairaanhoitopiirit.png)
 
 End
--- hs-koronavirus-avoindata/hs-koronavirus-avoindata.r
+++ hs-koronavirus-avoindata/hs-koronavirus-avoindata.r
@@ -1,5 +1,6 @@
 
 # @jussivirkkala
+# 2021-02-29 Renamed processedThlData, Adding finnishCoronaHospitalData
 # 2021-02-28 First version
 
 # Install package once.
@@ -22,29 +23,29 @@ y1<-unlist(data1[1])
 
 # Load data from hs-avoindata
 
-json<-fromJSON("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/processedThlData")
+processedThlData<-fromJSON("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/processedThlData")
 
 # Save json with ISO8601 date
 
-save(json,file=paste("hs-koronavirus-avoindata-",Sys.Date(),".RData",sep=""))
+save(processedThlData,file=paste("hs-koronavirus-avoindata-",Sys.Date(),".RData",sep=""))
 
 # Get all cases
-data<-as.data.frame(json$confirmed[22])
+data<-as.data.frame(processedThlData$confirmed[22])
 y<-unlist(data[1])
 
 # New case
 sum(y)-sum(y1)
 
 files<-c()
-for (i in 1:length(json$confirmed)) {
+for (i in 1:length(processedThlData$confirmed)) {
   # Convert to frame
-  data<-as.data.frame(json$confirmed[i])
+  data<-as.data.frame(processedThlData$confirmed[i])
   y<-unlist(data[1])
   data1<-as.data.frame(json1$confirmed[i])
   y1<-unlist(data1[1])
   
   
-  title1=names(json$confirmed[i])
+  title1=names(processedThlData$confirmed[i])
   title2=paste(title1," ",Sys.Date()," N=",sum(y),". Tänään ",sum(y)-sum(y1),".",sep="")
 
   # Plot
@@ -62,6 +63,7 @@ for (i in 1:length(json$confirmed)) {
 }
 
 # Create gif
+
 imgs <- files
 img_list <- lapply(imgs, image_read)
 img_joined <- image_join(img_list)
@@ -70,4 +72,5 @@ img_animated <- image_animate(img_joined, fps = 1)
 img_animated
 image_write(image = img_animated,path = "tapaukset.gif")
 
+# git commit -a -m "Update"
 # End
