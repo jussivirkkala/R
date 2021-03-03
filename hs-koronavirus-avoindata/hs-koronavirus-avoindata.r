09bcb9b62bec3b4d397c7988abce717263ae4b09
jussivirkkala <jussivirkkala@hotmail.com>
2021-03-03 17:20:01 +0200

    Update

--- hs-koronavirus-avoindata/hs-koronavirus-avoindata.r
+++ hs-koronavirus-avoindata/hs-koronavirus-avoindata.r
@@ -1,8 +1,14 @@
 
 # @jussivirkkala
+# 2021-03-03 Renamed axis, COVID-19
 # 2021-03-01 Subplot
 # 2021-02-29 Renamed processedThlData, Adding finnishCoronaHospitalData
 # 2021-02-28 First version
+
+# R version 4.0.4 (2021-02-15)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19042)
+
 
 # Install packages once.
 
@@ -46,11 +52,18 @@ for (i in 1:length(names)) {
   region<-finnishCoronaHospitalData$hospitalised %>% filter(area==n)
   hospitalised=region$totalHospitalised;
   inICU=region$inIcu;
-
-  title2=paste(n," ",Sys.Date()," Hospitalized ",hospitalised[length(hospitalised)]," in ICU ",inICU[length(inICU)],sep="")
+  deadDiff<-c(0,diff(region$dead))
+  
   png(file=paste("Hospital-",n,".png",sep=""),width=1000,heigh=500)
-  plot(hospitalised,type="l",xlab = "Entries",ylab="Hospitalized (in ICU)",main =title2 )
+
+  title2=paste(n," ",Sys.Date()," Sairaalassa ",hospitalised[length(hospitalised)],
+               " (",hospitalised[length(hospitalised)]-hospitalised[length(hospitalised)-1],")",
+               " teholla ",inICU[length(inICU)]," (",inICU[length(inICU)]-inICU[length(inICU)-1],")",sep="")
+  par(mfrow=c(2,1))
+  plot(hospitalised,type="l",xlab = "Tietoja",ylab="Sairaalassa, teholla",main =title2 )
   lines(inICU)
+  title3=paste("Kuolemia ",region$dead[length(region$dead)]," (",region$dead[length(region$dead)]-region$dead[length(region$dead)-1],")",sep="")
+  plot(deadDiff,type="l",xlab="Tietoja",ylab="Muutos kuolemissa",main=title3)
   dev.off()
 }
 
@@ -69,10 +82,8 @@ for (i in 1:length(processedThlData$confirmed)) {
   data1<-as.data.frame(as.data.frame(processedThlData1$confirmed[i]))
   y1<-unlist(data1[1])
   
-  
   title1=names(processedThlData$confirmed[i])
   title2=paste(title1," ",Sys.Date()," N=",sum(y),". Lisäys ",sum(y)-sum(y1),".",sep="")
-
 
   # Save
   title1<-paste("tapaukset-",title1,".png",sep="")
@@ -81,9 +92,9 @@ for (i in 1:length(processedThlData$confirmed)) {
   png(file=title1,width=1000,heigh=500)
   yl=c(0,max(y[1:(length(y)-3)]))
   par(mfrow=c(2,1))
-  plot(y[1:(length(y)-3)],type="l",lwd=2,ylab="Korona tapauksia",xlab="Päiviä 2020 alusta",main=title2,ylim=yl)
+  plot(y[1:(length(y)-3)],type="l",lwd=2,ylab="COVID-19 tapauksia",xlab="Päiviä 2020 alusta",main=title2,ylim=yl)
   abline(v=c((length(y)-3-28),(length(y)-3)),lwd=2)
-  plot(y[(length(y)-3-28):(length(y)-3)],type="b",lwd=2,ylab="Korona tapauksia",xlab="Viimeiset 28 päivää (poislukien uusimmat 4)",ylim=yl, new=FALSE)
+  plot(y[(length(y)-3-28):(length(y)-3)],type="b",lwd=2,ylab="COVID-19 tapauksia",xlab="Viimeiset 28 päivää (poislukien uusimmat 4)",ylim=yl, new=FALSE)
   abline(v=c(1,8,15,22,29),lwd=2)
   dev.off()
 }
